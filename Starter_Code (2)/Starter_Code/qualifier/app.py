fb99e4a4ba15372d600e80deae22579fcc30e970
mightlee123 <mightlee123@gmail.com>
2021-01-13 22:14:19 -0800

    save_csv

--- Starter_Code (2)/Starter_Code/qualifier/app.py
+++ Starter_Code (2)/Starter_Code/qualifier/app.py
@@ -9,6 +9,7 @@ Example:
 import sys
 import fire
 import questionary
+import csv
 from pathlib import Path
 
 from qualifier.utils.fileio import load_csv
@@ -22,6 +23,13 @@ from qualifier.filters.max_loan_size import filter_max_loan_size
 from qualifier.filters.credit_score import filter_credit_score
 from qualifier.filters.debt_to_income import filter_debt_to_income
 from qualifier.filters.loan_to_value import filter_loan_to_value
+
+def save_csv(qualifying_loans):
+    output_path=Path("qualifying_loans.csv")
+    with open(output_path, "w", newline="") as csvfile:
+        csvwriter= csv.writer(csvfile)
+        for i in qualifying_loans:
+            csvwriter.writerow(value.values())
 
 
 def load_bank_data():
@@ -110,6 +118,7 @@ def save_qualifying_loans(qualifying_loans):
     """
     # @TODO: Complete the usability dialog for savings the CSV Files.
     # YOUR CODE HERE!
+    
 
 
 def run():
